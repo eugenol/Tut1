50b6a0ff139e3b3296434e718b1393596b1b1eb2
Eugene <eug@live.com>
2015-02-25 15:56:00 +0200

    add personal greeting

--- Tut1/hello.cpp
+++ Tut1/hello.cpp
@@ -1,9 +1,15 @@
 // John Poole, CM3, 25/02/2015
 // My first C++ program (Hello, World!)
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() {
-	cout << "Hello, World!" << endl;
+	cout << "Enter your name: " << endl;
+	string myName;
+	cin >> myName;
+	cout << "Hello, " << myName << endl;
+	cout << "Goodbye";
+	return 0;
 }
